cc782825ea9a5d92973054315db3a9cbcff5b252
ssbaytri <azjoker333@gmail.com>
2026-08-14 21:51:07 +0100

    setup

---
+++ ex00/Cinema/src/main/java/fr/_42/cinema/CinemaApplication.java
@@ -0,0 +1,13 @@
+package fr._42.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaApplication.class, args);
+    }
+
+}
---
+++ ex00/Cinema/src/test/java/fr/_42/cinema/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr._42.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
